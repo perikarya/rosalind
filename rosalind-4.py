22515b024f27ff4112bc70cffe486d610b899759
perikarya <40226185+perikarya@users.noreply.github.com>
2020-08-03 19:59:25 +0800

    Update rosalind-4.py

--- rosalind-4.py
+++ rosalind-4.py
@@ -1,3 +1,6 @@
+''' rabbits and recurrence relations: given positive integers n ≤ 40 and k ≤ 5, returns the total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs, where the number of rabbit pairs alive after the nth month is defined by defined by the recurrence relation Fn = Fn1 + Fn2 (with F1 = F2 = 1 to initiate the sequence)
+'''
+
 f = open("rosalind_fib.txt", "r") 
 
 a, b = f.readline().strip().split(" ")
