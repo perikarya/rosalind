3ffa86352a4910966bd6da707e14c6cfcf92ec77
perikarya <40226185+perikarya@users.noreply.github.com>
2020-08-03 19:55:15 +0800

    Update rosalind-2.py

--- rosalind-2.py
+++ rosalind-2.py
@@ -1,3 +1,6 @@
+''' transcribing DNA into RNA: given a DNA string t with max length 1000 nt, returns the transcribed RNA string of t
+'''
+
 f = open("rosalind_rna.txt", "r") 
 
 t = f.readline().strip()
